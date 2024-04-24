54db2bc0f3dfd3bf71ce6b659c28e85a68bf8d11
nkosko21 <nickoy7@gmail.com>
2024-04-24 10:42:51 -0400

    bugs

--- App.js
+++ App.js
@@ -3,16 +3,20 @@ import session from "express-session";
 import mongoose from "mongoose";
 import cors from "cors";
 import AccountRoutes from "./Database/Account/routes.js";
+import UserRoutes from "./Database/Product/productRoutes.js";
 
-
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/uniswap"
+const CONNECTION_STRING = 'mongodb+srv://uniswap-admin:strong-password@uniswap.cb81hea.mongodb.net/' // || "mongodb://127.0.0.1:27017/uniswap"
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 // app.use(cors({
 //     credentials: true,
 //     origin: process.env.FRONTEND_URL
 // }));
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
 const sessionOptions = {
     secret: "secret",
     resave: false,
@@ -29,4 +33,5 @@ if (process.env.NODE_ENV !== "development") {
 app.use(session(sessionOptions));
 app.use(express.json());
 AccountRoutes(app);
+UserRoutes(app);
 app.listen(4000);
